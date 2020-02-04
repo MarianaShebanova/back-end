69191d25f39f591b722e2e3769fba75347ebdc4e
MarianaShebanova <mariana.shebanova@gmail.com>
2020-02-03 16:18:11 -0800

    MVP

---
+++ projects/projectsRouter.js
@@ -0,0 +1,93 @@
+const express = require('express');
+
+const Project = require('./project-model');
+
+const router = express.Router();
+
+const Users = require("../users/users-model.js");
+
+router.get('/', (req, res) => {
+    // read the data from the database
+    Project.find() // return a promise
+        .then(data => {
+            res.status(200).json(data);
+        })
+        .catch(error => {
+            console.log(error);
+            // handle the error
+            res.status(500).json({
+                errorMessage: 'The Account information could not be retrieved.',
+            });
+        });
+});
+
+router.get('/read', (req, res) => {
+    let { email, password } = req.body;
+    Users.findBy({ email })
+        .first()
+        .then(user => {
+            Project.findByUserId(user.id) // return a promise
+                .then(data => {
+                    res.status(200).json(data);
+                })
+                .catch(error => {
+                    console.log(error);
+                    // handle the error
+                    res.status(500).json({
+                        errorMessage: 'The Account information could not be retrieved.',
+                    });
+                });
+            
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+router.get('/read/:id', (req, res) => {
+    const { id } = req.params;
+    Project.findById(id)
+        .then(data => {
+            if (data.length != 0) {
+                res.status(200).json(data);
+            } else {
+                return res.status(400).json({
+                    errorMessage: "The Account with ID could not be retrieved."
+                });
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                errorMessage: "The Account information could not be retrieved."
+            });
+        })
+});
+
+router.post('/create', (req, res) => {
+    const changes = req.body;
+    let email = req.body.email;
+    Users.findBy({ email })
+        .first()
+        .then(user => {
+            console.log("AAAA");
+            changes.user_id = user.id;
+            Projects.add(changes)
+                .then(data => {
+                    console.log(data);
+                    res.status(201).json(data);
+                })
+                .catch(error => {
+                    console.log(error);
+                    res.status(500).json({
+                        errorMessage: 'sorry, we ran into an error creating the Account',
+                    });
+                });
+
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+module.exports = router;
